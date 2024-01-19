901bbe02bb5306708d3d39e136e639b7d9870195
drspchoi <drspchoi@gmail.com>
2024-01-19 11:56:01 -0800

    Update if three input is added, display show the result. Now, I need to make it calculate when I click enter

--- java.js
+++ java.js
@@ -31,6 +31,26 @@ function operation (value1, value2, operator) {
         return divide(value1,value2)
     }
 }
+function display() {
+    const display=document.querySelectorAll('button')
+    const input=[]
+    display.forEach((button) => {
+        button.addEventListener('click', ()=> {
+            document.querySelector('.display').textContent=`${button.id}`
+            input.push(button.id)
+            console.log(input)
+            if (input.length === 3) {
+                const output=operation(Number(input[0]),Number(input[2]),input[1])
+                console.log(output)
+                document.querySelector('.display').textContent=`${output}`
+                input.length=0;
+                input.push(output)
+                console.log(input)
+            } 
+        })
+    })
+}
 
 const result=operation(value1,value2,operator)
 console.log(result)
+display()
